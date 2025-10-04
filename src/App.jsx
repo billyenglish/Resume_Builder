3dcdf6f54eba47598783131926a0cbb19e405af2
Billy English <billyenglish6212@outlook.com>
2025-10-04 11:43:08 -0400

    Creating Routing for Navigation to different links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './components/Homepage'
+import PersonalDetails from './components/PersonalDetails'
+import Links from "./components/Links";
+import TechnicialSkills from "./components/TechnicialSkills";
+import WorkExperience from './components/WorkExperience';
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Certificate from './components/Certificates';
 
 function App() {
 
@@ -7,7 +14,14 @@ function App() {
     <>
       <Routes>
         <Route index element={<Homepage />} />
-      </Routes>   
+        <Route path="/personal_details" element={<PersonalDetails />} />
+        <Route path="/links" element={<Links />} />
+        <Route path="/technicial_skills" element={<TechnicialSkills />} />
+        <Route path="/work_experience" element={<WorkExperience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/certificate" element={<Certificate />} />
+      </Routes> 
     </>
   )
 }
