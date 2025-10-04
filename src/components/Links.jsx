348f6c244de27f6e6a158f83b8725c3c2a0fdac4
Billy English <billyenglish6212@outlook.com>
2025-10-04 11:40:37 -0400

    Create Links Section Pages.

---
+++ src/components/Links.jsx
@@ -0,0 +1,59 @@
+import Nav from "./Nav"
+import Footer from "./Footer"
+import Button from "../subcomponents/Button"
+import InputText from "../subcomponents/InputText";
+import "../styles/links.css";
+
+const Links = () => {
+
+    return (
+        <>
+            <Nav />
+            <section className="links_section section">
+                <form>
+                    <div className="form_container">
+                        <div className="form_header">
+                            <h2 className="form_title">
+                                Links
+                            </h2>
+                            
+                            <div className="form_control_panel">
+                                <Button
+                                    buttonText="Back"
+                                />
+                                <Button
+                                    buttonText="Next"
+                                />
+                            </div>
+                        </div>
+                        <div className="form_rows form_links">
+                            <h3 className="category_title">Website:</h3>
+                            <div className="form_catergory">
+                                <label
+                                    htmlFor="url"
+                                    className="form_label"
+                                >
+                                    URL:
+                                    <InputText
+                                        id="url"
+                                    />
+                                </label>
+                            
+                                <label
+                                    htmlFor="website_username"
+                                    className="form_label"
+                                >
+                                    Username:
+                                    <InputText />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default Links;
